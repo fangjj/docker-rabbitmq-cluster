812d40d2b89410b8fd6660c156216b1c1b314edd
jm <jm@AludeMacBook-Pro.local>
2018-03-15 11:29:29 +0800

    remove tag

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,2 @@
-TAG="3.7.0"
-
-docker build -t fangjimjim/rabbitmq-base:$TAG base
-docker build -t fangjimjim/rabbitmq-server:$TAG server
+docker build -t fangjimjim/rabbitmq-base base
+docker build -t fangjimjim/rabbitmq-server server
